265bf31fc8b238a614178658214fca948f7e300a
Sergey <sv0lapin@gmail.com>
2015-01-09 19:36:25 +0300

    injecting js

--- gulpfile.js
+++ gulpfile.js
@@ -3,16 +3,12 @@
 var gulp = require('gulp');
 var gulpLess = require('gulp-less');
 var gulpConcat = require('gulp-concat');
+var gulpInject = require('gulp-inject');
+var gulpUglify = require('gulp-uglify');
 
 var distPath = function() {
   return './public/dist';
 };
-
-gulp.task('html', function() {
-  // copy index.html to public
-  gulp.src('ui/index.html')
-    .pipe(gulp.dest('./public'));
-});
 
 gulp.task('less', function() {
   return gulp.src(['ui/components/bootstrap-less/less/bootstrap.less',
@@ -22,9 +18,33 @@ gulp.task('less', function() {
     .pipe(gulp.dest(distPath() + '/css'));
 });
 
+gulp.task('js-libs-dev', function() {
+  return gulp.src(['ui/components/angular/angular.js'])
+    .pipe(gulpConcat('libs.js'))
+    .pipe(gulp.dest(distPath() + '/js'));
+});
+
+gulp.task('index', function() {
+  var target = gulp.src('ui/index.html');
+
+  var jsStream = gulp.src(['ui/ng/app.module.js']);
+
+  if (process.env.NODE_ENV === 'production') {
+    jsStream = jsStream
+      .pipe(gulpConcat('app.js'))
+      .pipe(gulpUglify());
+  }
+
+  jsStream.pipe(gulp.dest(distPath() + '/js'));
+
+  target
+    .pipe(gulpInject(jsStream, {ignorePath: 'public'}))
+    .pipe(gulp.dest('./public'));
+});
+
 gulp.task('build', ['html', 'less'], function() {});
 
-gulp.task('watch', ['html', 'less'], function() {
+gulp.task('watch', ['index', 'less', 'js-libs-dev'], function() {
   gulp.watch('ui/assets/less/**/*.less', ['less']);
-  gulp.watch('ui/index.html', ['html']);
+  gulp.watch('ui/index.html', ['index']);
 });
---
+++ ui/ng/app.module.js
@@ -0,0 +1,3 @@
+angular.module('test-module', function() {
+  alert('hello');
+});
